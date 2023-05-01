18e71f8395d3efda98f532d02b52c0881a0b32a6
HHIEPZ <tranhuuhiep2004@gmail.com>
2023-05-02 01:52:41 +0700

    Upload Source

--- assets/js/header.js
+++
@@ -1,6 +0,0 @@
-// let subMenu = document.querySelector("#js-headerSubMenu");
-
-// subMenu.addEventListener('click', () => {
-//     let subMenuUl = subMenu.querySelector('ul');
-//     subMenuUl.classList.toggle('active');
-// });
